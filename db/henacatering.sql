ea43d4523f8981c4338da9aa39a76cc92b25b21b
Vebe <fikri1838@gmail.com>
2023-02-10 01:42:28 +0700

    Table Transaksi SQL

--- db/henacatering.sql
+++ db/henacatering.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2023 at 09:15 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Generation Time: Feb 09, 2023 at 06:48 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,23 @@ SET time_zone = "+00:00";
 --
 -- Database: `henacatering`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `transaksi_id` int(11) NOT NULL,
+  `transaksi_tanggal` date NOT NULL,
+  `transaksi_jenis` enum('Pengeluaran','Pemasukan') CHARACTER SET utf8 NOT NULL,
+  `transaksi_kategori` int(11) NOT NULL,
+  `transaksi_nominal` int(11) NOT NULL,
+  `transaksi_keterangan` text CHARACTER SET utf8 NOT NULL,
+  `transaksi_foto` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `transaksi_bank` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -50,6 +67,12 @@ INSERT INTO `user` (`userid`, `email`, `password`, `role`) VALUES
 --
 
 --
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`transaksi_id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -58,6 +81,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `transaksi_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
