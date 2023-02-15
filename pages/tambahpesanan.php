deb6c2d82d95c685805e6d16162be20ceaac2e27
TB Nawaf Musyaffa <iatmanegara.22@gmail.com>
2023-02-15 18:59:28 +0700

    Update tambahpesanan.php
    - perbaikan ukuran column
    - perbaikan fungsi button
    - menghilangkan </br>
    -

--- pages/tambahpesanan.php
+++ pages/tambahpesanan.php
@@ -26,60 +26,24 @@ if (isset($_POST['btnSubmit'])) {
   }
 }
 ?>
-<div class="container">
-  <!-- <div class="col-md-6">
-    <h3>Tambah Pesanan</h3>
-    <form class="form-horizontal" action="" method="post">
-      <div class="form-group">
-        <label class="control-label col-sm-2" for="pesanan">pesanan:</label>
-        <div class="col-sm-10">
-          <input type="pesanan" class="form-control" id="pesanan" placeholder="Enter pesanan" name="pesanan">
-        </div>
-      </div> -->
 
-     <!-- <div class="form-group">
-        <label class="control-label col-sm-2" for="password">Password:</label>
-        <div class="col-sm-10">
-          <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
-        </div>
-      </div>
-      <div class="form-group">
-        <label class="control-label col-sm-2" for="role">Peran:</label>
-        <div class="col-sm-10">
-          <select name="role" require class="form-control" required>
-            <?php
-            $arrayRole = array("owner", "admin");
-            foreach ($arrayRole as $selectedRole) {
-              echo '<option value="' . $selectedRole . '">' . $selectedRole . '</option>';
-            }
-            ?>
-          </select>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-          <input type="submit" class="btn btn-success" value="Simpan" name="btnSubmit">
-          <a href="index.php?p=lihatuser" class="btn btn-danger">Batal</a></td>
-        </div>
-      </div>-->
-      
-      
-      <!--Test Kolum Baru-->
-      <div class="container">
+  
+    <div class="container">
+      <form class="form-horizontal" action="" method="post">
         <div class="col-md-6"><!--COL Kiri-->
           <div class="row"><!--ROW 1-->
             <div class="form-group"> 
-              <label class="control-label col-sm-2" for="id_pesanan">ID Pesanan:</label>
-                <div class="col-sm-4">
+              <label class="control-label col-sm-5" for="id_pesanan">ID Pesanan:</label>
+                <div class="col-sm-7">
                 <input type="text" class="form-control" id="id_pesanan" name="id_pesanan" value="01" readonly>
                 </div>
               </div>
           </div>
-          </br>
+          
           <div class="row"><!--ROW 2-->
             <div class="form-group"> 
-              <label class="control-label col-sm-2" for="id_pelanggan">Pelanggan:</label>
-                <div class="col-sm-4">
+              <label class="control-label col-sm-5" for="id_pelanggan">Pelanggan:</label>
+                <div class="col-sm-7">
                   <select class="form-control" id="id_pelanggan" name="id_pelanggan">
                   <option value="1">Pemkot Depok</option>
                   <option value="2">Muhammad Sumbul</option>
@@ -88,38 +52,38 @@ if (isset($_POST['btnSubmit'])) {
                 </div>
             </div>
           </div>
-          </br>
+          
           <div class="row"><!--ROW 3-->
             <div class="form-group"> 
-              <label class="control-label col-sm-2" for="alamat_pelanggan">Alamat:</label>
-                <div class="col-sm-4">
+              <label class="control-label col-sm-5" for="alamat_pelanggan">Alamat:</label>
+                <div class="col-sm-7">
                 <textarea class="form-control" id="alamat_pelanggan" placeholder="Alamat" name="alamat_pelanggan" rows="2" cols="20"></textarea>
               </div>
             </div>
           </div>
-          </br>
+          
             <div class="row"><!--ROW 4-->
               <div class="form-group"> 
-                <label class="control-label col-sm-2" for="nama_pelanggan">Nama:</label>
-                <div class="col-sm-4">
+                <label class="control-label col-sm-5" for="nama_pelanggan">Nama:</label>
+                <div class="col-sm-7">
                   <input type="text" class="form-control" id="nama_pelanggan" placeholder="Nama" name="nama_pelanggan" required>
               </div>
             </div>
           </div>
-          </br>
+          
           <div class="row"><!--ROW 5-->
             <div class="form-group"> 
-                <label class="control-label col-sm-2" for="no_hp">No. HP:</label>
-                <div class="col-sm-4">
+                <label class="control-label col-sm-5" for="no_hp">No. HP:</label>
+                <div class="col-sm-7">
                 <input type="number" class="form-control" id="no_hp" placeholder="Masukkan No. Hp" name="no_hp" required>
                 </div>
             </div>
           </div>
-          </br>
+          
           <div class="row"><!--ROW 6-->
             <div class="form-group"> 
-              <label class="control-label col-sm-2" for="catatan">Catatan:</label>
-              <div class="col-sm-4">
+              <label class="control-label col-sm-5" for="catatan">Catatan:</label>
+              <div class="col-sm-7">
                 <textarea class="form-control" id="catatan" placeholder="catatan" name="catatan" rows="2" cols="20"></textarea>
               </div>
             </div>
@@ -128,17 +92,17 @@ if (isset($_POST['btnSubmit'])) {
         <div class="col-md-6"><!--COL Kanan-->
           <div class="row"><!--ROW 1-->
               <div class="form-group"> 
-                <label class="control-label col-sm-2" for="tanggal_pesanan">Tanggal Pesanan:</label>
-                <div class="col-sm-4">
+                <label class="control-label col-sm-5" for="tanggal_pesanan">Tanggal Pesanan:</label>
+                <div class="col-sm-7">
                   <input type="date" class="form-control" id="tanggal_pesanan" name="tanggal_pesanan">
               </div>
             </div>
           </div>
-          </br>
+          
           <div class="row"><!--ROW 2-->
             <div class="form-group"> 
-              <label class="control-label col-sm-2" for="id_item_pesanan">Jenis Pesanan:</label>
-                <div class="col-sm-4">
+              <label class="control-label col-sm-5" for="id_item_pesanan">Jenis Pesanan:</label>
+                <div class="col-sm-7">
                   <select class="form-control" id="id_item_pesanan" name="id_item_pesanan">
                   <option value="1">Snack Box</option>
                   <option value="2">Nasi Box</option>
@@ -147,55 +111,55 @@ if (isset($_POST['btnSubmit'])) {
                 </div><!--sampel dropdown-->
               </div>
           </div>
-          </br>
+          
           <div class="row"><!--ROW 3-->
             <div class="form-group"> 
-              <label class="control-label col-sm-2" for="jumlah_pesanan">Jumlah Pesanan:</label>
-                <div class="col-sm-4">
+              <label class="control-label col-sm-5" for="jumlah_pesanan">Jumlah Pesanan:</label>
+                <div class="col-sm-7">
                   <input type="number" class="form-control" id="jumlah_pesanan" name="jumlah_pesanan">
               </div>
             </div>
           </div>
-          </br>
+          
             <div class="row"><!--ROW 4-->
               <div class="form-group"> 
-                <label class="control-label col-sm-2" for="subtotal_pesanan">subtotal pesanan:</label>
-                  <div class="col-sm-4">
+                <label class="control-label col-sm-5" for="subtotal_pesanan">subtotal pesanan:</label>
+                  <div class="col-sm-7">
                   <input type="text" class="form-control" id="subtotal_pesanan" name="subtotal_pesanan" readonly>
               </div>
             </div>
           </div>
-          </br>
+          
           <div class="row"><!--ROW 5-->
             <div class="form-group"> 
-              <label class="control-label col-sm-2" for="besar_pajak">pajak pesanan:</label>
-              <div class="col-sm-4">
-                <input type="text" class="form-control" id="besar_pajak" name="besar_pajak" value="10%" readonly>
+              <label class="control-label col-sm-5" for="besar_pajak">pajak pesanan:</label>
+              <div class="col-sm-7">
+                <input type="text" class="form-control" id="besar_pajak" name="besar_pajak" value=0 readonly>
               </div>
             </div>
           </div>
-          </br>
+          
           <div class="row"><!--ROW 6-->
             <div class="form-group"> 
-              <label class="control-label col-sm-2" for="pajak_pesanan"></label>
-              <div class="col-sm-4">
-                <input type="text" class="form-control" id="pajak_pesanan" name="pajak_pesanan" value="10%*harga makanan" readonly>
+              <label class="control-label col-sm-5" for="pajak_pesanan"></label>
+              <div class="col-sm-7">
+                <input type="text" class="form-control" id="pajak_pesanan" name="pajak_pesanan" value=0 readonly>
               </div>
             </div>
           </div>
-          </br>
+          
           <div class="row"><!--ROW 7-->
             <div class="form-group"> 
-              <label class="control-label col-sm-2" for="total_pesanan">total pesanan:</label>
-              <div class="col-sm-4">
+              <label class="control-label col-sm-5" for="total_pesanan">total pesanan:</label>
+              <div class="col-sm-7">
               <input type="text" class="form-control" id="total_pesanan" name="total_pesanan" readonly>
               </div>
             </div>
           </div>
-          </br>
+          </br> 
           <div class="row"><!--ROW 8-->
             <div class="form-group">
-              <div class="col-sm-offset-2 col-sm-10">
+              <div class="col-sm-offset-5 col-sm-7">
               <input type="submit" class="btn btn-success" value="Simpan" name="btnSubmit">
               <a href="index.php?p=lihatuser" class="btn btn-danger">Batal</a></td>
               </div>
@@ -206,4 +170,3 @@ if (isset($_POST['btnSubmit'])) {
 
     </form>
   </div>
-</div>
