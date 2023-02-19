676894ff46e3a6b3b869fc7a9ab1980f6d2f9695
Vebe <fikri1838@gmail.com>
2023-02-19 11:52:58 +0700

    Update ubah pesanan

--- pages/ubahpesanan.php
+++ pages/ubahpesanan.php
@@ -14,135 +14,135 @@ if (isset($_POST['btnSubmit'])) {
   if ($objPesanan->hasil) {
     echo '<script> window.location="index.php?p=lihatpesanan"; </script>';
   }
-} else if (isset($_GET['userid'])) {
-  $objPesanan->userid = $_GET['userid'];
+} else if (isset($_GET['id_pesanan'])) {
+  $objPesanan->id_pesanan = $_GET['id_pesanan'];
   $objPesanan->LihatSatuPesanan();
 }
 ?>
 
-  
+
 <section class="content-header">
   <h1>
-	Pesanan
-	<small>Data Pesanan</small>
+    Pesanan
+    <small>Data Pesanan</small>
   </h1>
   <ol class="breadcrumb">
-	<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-	<li class="active">Dashboard</li>
+    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li class="active">Dashboard</li>
   </ol>
 </section>
 
 <section class="content">
   <div class="row">
-	<section class="col-lg-12">
-	  <div class="box box-info">
+    <section class="col-lg-12">
+      <div class="box box-info">
 
-		<div class="box-header">
-		  <h3 class="box-title">Ubah Pesanan</h3>		
-		</div>
-		<div class="box-body">
-        
-      <form class="form-horizontal" action="" method="post">
-        <div class="col-md-5"><!--COL Kiri-->
-          <div class="row"><!--ROW 1-->
-            <div class="form-group"> 
-              <label class="control-label col-sm-5" for="id_pesanan">ID Pesanan:</label>
-                <div class="col-sm-7">
-                <input type="text" class="form-control" id="id_pesanan" name="id_pesanan" value="<?php echo $objPesanan->id_pesanan; ?>" readonly>
-                </div>
-              </div>
-          </div>
-          
-          <div class="row"><!--ROW 2-->
-            <div class="form-group"> 
-              <label class="control-label col-sm-5" for="id_pelanggan">Pelanggan:</label>
-                <div class="col-sm-7">
-                  <select class="form-control" id="id_pelanggan" name="id_pelanggan" value="<?php echo $objPesanan->id_pelanggan; ?>" readonly>
-                  <option value="1">Pemkot Depok</option>
-                  <option value="2">Muhammad Sumbul</option>
-                  <option value="3">ESQ 165</option>
-                  </select>
-                </div>
-            </div>
-          </div>
-          
-          <div class="row"><!--ROW 3-->
-            <div class="form-group"> 
-              <label class="control-label col-sm-5" for="alamat_pelanggan">Alamat:</label>
-                <div class="col-sm-7">
-                <textarea class="form-control" id="alamat_pelanggan" placeholder="Alamat" name="alamat_pelanggan" rows="2" cols="20" required readonly><?php echo $objPesanan->alamat_pelanggan; ?></textarea>
-              </div>
-            </div>
-          </div>
-          
-            <div class="row"><!--ROW 4-->
-              <div class="form-group"> 
-                <label class="control-label col-sm-5" for="nama_pelanggan">Nama:</label>
-                <div class="col-sm-7">
-                  <input type="text" class="form-control" id="nama_pelanggan" placeholder="Nama" name="nama_pelanggan" value="<?php echo $objPesanan->nama_pelanggan; ?>" required readonly>
-              </div>
-            </div>
-          </div>
-          
-          <div class="row"><!--ROW 5-->
-            <div class="form-group"> 
-                <label class="control-label col-sm-5" for="no_hp">No. HP:</label>
-                <div class="col-sm-7">
-                <input type="number" class="form-control" id="no_hp" placeholder="Masukkan No. Hp" name="no_hp" value="<?php echo $objPesanan->no_hp; ?>" required readonly>
-                </div>
-            </div>
-          </div>
-          
-          <div class="row"><!--ROW 6-->
-            <div class="form-group"> 
-              <label class="control-label col-sm-5" for="catatan">Catatan:</label>
-              <div class="col-sm-7">
-                <textarea class="form-control" id="catatan" placeholder="catatan" name="catatan" rows="2" cols="20"><?php echo $objPesanan->catatan; ?></textarea>
-              </div>
-            </div>
-          </div>
+        <div class="box-header">
+          <h3 class="box-title">Ubah Pesanan</h3>
         </div>
-        <div class="col-md-5"><!--COL Kanan-->
-          <div class="row"><!--ROW 1-->
-              <div class="form-group"> 
-                <label class="control-label col-sm-5" for="tanggal_pesanan">Tanggal Pesanan:</label>
-                <div class="col-sm-7">
-                  <input type="date" class="form-control" id="tanggal_pesanan" name="tanggal_pesanan" value="<?php echo $objPesanan->tanggal_pesanan; ?>" required>
-              </div>
-            </div>
-          </div>
-          
-          <div class="row"><!--ROW 2-->
-            <div class="form-group"> 
-              <label class="control-label col-sm-5" for="id_item_pesanan">Jenis Pesanan:</label>
-                <div class="col-sm-7">
-                  <select class="form-control" id="id_item_pesanan" name="id_item_pesanan" value="<?php echo $objPesanan->id_item_pesanan; ?>" onchange="hitung()">
-                  <option value="1">Snack Box</option>
-                  <option value="2">Nasi Box</option>
-                  <option value="3">Prasmanan</option>
-                  </select>
-                </div><!--sampel dropdown-->
-              </div>
-          </div>
-          
-          <div class="row"><!--ROW 3-->
-            <div class="form-group"> 
-              <label class="control-label col-sm-5" for="jumlah_pesanan">Jumlah Pesanan:</label>
-                <div class="col-sm-7">
-                  <input type="number" class="form-control" id="jumlah_pesanan" name="jumlah_pesanan" value="<?php echo $objPesanan->jumlah_pesanan; ?>" onchange="hitung()" required>
-              </div>
-            </div>
-          </div>
-          
-            <div class="row"><!--ROW 4-->
-              <div class="form-group"> 
-                <label class="control-label col-sm-5" for="subtotal_pesanan">subtotal pesanan:</label>
+        <div class="box-body">
+
+          <form class="form-horizontal" action="" method="post">
+            <div class="col-md-5"><!--COL Kiri-->
+              <div class="row"><!--ROW 1-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="id_pesanan">ID Pesanan:</label>
                   <div class="col-sm-7">
-                  <input type="text" class="form-control" id="subtotal_pesanan" name="subtotal_pesanan" value="<?php echo $objPesanan->subtotal_pesanan; ?>" readonly>
+                    <input type="text" class="form-control" id="id_pesanan" name="id_pesanan" value="<?php echo $objPesanan->id_pesanan; ?>" readonly>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row"><!--ROW 2-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="id_pelanggan">Pelanggan:</label>
+                  <div class="col-sm-7">
+                    <select class="form-control" id="id_pelanggan" name="id_pelanggan" value="<?php echo $objPesanan->id_pelanggan; ?>" readonly>
+                      <option value="1">Pemkot Depok</option>
+                      <option value="2">Muhammad Sumbul</option>
+                      <option value="3">ESQ 165</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row"><!--ROW 3-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="alamat_pelanggan">Alamat:</label>
+                  <div class="col-sm-7">
+                    <textarea class="form-control" id="alamat_pelanggan" placeholder="Alamat" name="alamat_pelanggan" rows="2" cols="20" required readonly><?php echo $objPesanan->alamat_pelanggan; ?></textarea>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row"><!--ROW 4-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="nama_pelanggan">Nama:</label>
+                  <div class="col-sm-7">
+                    <input type="text" class="form-control" id="nama_pelanggan" placeholder="Nama" name="nama_pelanggan" value="<?php echo $objPesanan->nama_pelanggan; ?>" required readonly>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row"><!--ROW 5-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="no_hp">No. HP:</label>
+                  <div class="col-sm-7">
+                    <input type="number" class="form-control" id="no_hp" placeholder="Masukkan No. Hp" name="no_hp" value="<?php echo $objPesanan->no_hp; ?>" required readonly>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row"><!--ROW 6-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="catatan">Catatan:</label>
+                  <div class="col-sm-7">
+                    <textarea class="form-control" id="catatan" placeholder="catatan" name="catatan" rows="2" cols="20"><?php echo $objPesanan->catatan; ?></textarea>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <!--ROW 5
+            <div class="col-md-5"><!--COL Kanan-->
+              <div class="row"><!--ROW 1-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="tanggal_pesanan">Tanggal Pesanan:</label>
+                  <div class="col-sm-7">
+                    <input type="date" class="form-control" id="tanggal_pesanan" name="tanggal_pesanan" value="<?php echo $objPesanan->tanggal_pesanan; ?>" required>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row"><!--ROW 2-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="id_item_pesanan">Jenis Pesanan:</label>
+                  <div class="col-sm-7">
+                    <select class="form-control" id="id_item_pesanan" name="id_item_pesanan" value="<?php echo $objPesanan->id_item_pesanan; ?>" onchange="hitung()">
+                      <option value="1">Snack Box</option>
+                      <option value="2">Nasi Box</option>
+                      <option value="3">Prasmanan</option>
+                    </select>
+                  </div><!--sampel dropdown-->
+                </div>
+              </div>
+
+              <div class="row"><!--ROW 3-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="jumlah_pesanan">Jumlah Pesanan:</label>
+                  <div class="col-sm-7">
+                    <input type="number" class="form-control" id="jumlah_pesanan" name="jumlah_pesanan" value="<?php echo $objPesanan->jumlah_pesanan; ?>" onchange="hitung()" required>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row"><!--ROW 4-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="subtotal_pesanan">subtotal pesanan:</label>
+                  <div class="col-sm-7">
+                    <input type="text" class="form-control" id="subtotal_pesanan" name="subtotal_pesanan" value="<?php echo $objPesanan->subtotal_pesanan; ?>" readonly>
+                  </div>
+                </div>
+              </div>
+              <!--ROW 5
           <div class="row">
             <div class="form-group"> 
               <label class="control-label col-sm-5" for="besar_pajak">pajak pesanan:</label>
@@ -152,64 +152,64 @@ if (isset($_POST['btnSubmit'])) {
             </div>
           </div>
           -->
-          <div class="row"><!--ROW 6-->
-            <div class="form-group"> 
-              <label class="control-label col-sm-5" for="pajak_pesanan">pajak pesanan</label>
-              <div class="col-sm-7">
-                <input type="text" class="form-control" id="pajak_pesanan" name="pajak_pesanan" value="<?php echo $objPesanan->pajak_pesanan; ?>" readonly> 
+              <div class="row"><!--ROW 6-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="pajak_pesanan">pajak pesanan</label>
+                  <div class="col-sm-7">
+                    <input type="text" class="form-control" id="pajak_pesanan" name="pajak_pesanan" value="<?php echo $objPesanan->pajak_pesanan; ?>" readonly>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          
-          <div class="row"><!--ROW 7-->
-            <div class="form-group"> 
-              <label class="control-label col-sm-5" for="total_pesanan">total pesanan:</label>
-              <div class="col-sm-7">
-              <input type="text" class="form-control" id="total_pesanan" name="total_pesanan" value="<?php echo $objPesanan->total_pesanan; ?>" readonly>
-              </div>
-            </div>
-          </div>
-          </br> 
-          <div class="row"><!--ROW 8-->
-            <div class="form-group">
-              <div class="col-sm-offset-5 col-sm-7">
-              <input type="submit" class="btn btn-success" value="Simpan" name="btnSubmit">
-              <a href="dashboard.php?p=lihatpesanan" class="btn btn-danger">Batal</a></td>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
 
-    </form>  
-	
-		</div>
-	  </div>
-	</section>
+              <div class="row"><!--ROW 7-->
+                <div class="form-group">
+                  <label class="control-label col-sm-5" for="total_pesanan">total pesanan:</label>
+                  <div class="col-sm-7">
+                    <input type="text" class="form-control" id="total_pesanan" name="total_pesanan" value="<?php echo $objPesanan->total_pesanan; ?>" readonly>
+                  </div>
+                </div>
+              </div>
+              </br>
+              <div class="row"><!--ROW 8-->
+                <div class="form-group">
+                  <div class="col-sm-offset-5 col-sm-7">
+                    <input type="submit" class="btn btn-success" value="Simpan" name="btnSubmit">
+                    <a href="dashboard.php?p=lihatpesanan" class="btn btn-danger">Batal</a></td>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        </form>
+
+      </div>
   </div>
+</section>
+</div>
 </section>
 
 <script>
-function hitung() {
-	    var id_item_pesanan = document.getElementById("id_item_pesanan").value;
-      var jumlah_pesanan = document.getElementById("jumlah_pesanan").value;
-      var subtotal = 0;
-      if (id_item_pesanan == 1){
-        subtotal = 15000*jumlah_pesanan
-      }else if (id_item_pesanan == 2){
-        subtotal=25000*jumlah_pesanan
-      }else if(id_item_pesanan == 3){
-        subtotal = 50000*jumlah_pesanan
-      }
-      var besar_pajak = 10;//document.getElementById("besar_pajak").value;
-      var pajak = besar_pajak / 100 * subtotal;
-      
-      var total = subtotal + pajak;
+  function hitung() {
+    var id_item_pesanan = document.getElementById("id_item_pesanan").value;
+    var jumlah_pesanan = document.getElementById("jumlah_pesanan").value;
+    var subtotal = 0;
+    if (id_item_pesanan == 1) {
+      subtotal = 15000 * jumlah_pesanan
+    } else if (id_item_pesanan == 2) {
+      subtotal = 25000 * jumlah_pesanan
+    } else if (id_item_pesanan == 3) {
+      subtotal = 50000 * jumlah_pesanan
+    }
+    var besar_pajak = 10; //document.getElementById("besar_pajak").value;
+    var pajak = besar_pajak / 100 * subtotal;
 
-	    document.getElementById("subtotal_pesanan").value = subtotal;
-      document.getElementById("pajak_pesanan").value = pajak;
-      document.getElementById("total_pesanan").value = total;
-	}
+    var total = subtotal + pajak;
+
+    document.getElementById("subtotal_pesanan").value = subtotal;
+    document.getElementById("pajak_pesanan").value = pajak;
+    document.getElementById("total_pesanan").value = total;
+  }
 </script>
 Footer
 © 2023 GitHub, Inc.
