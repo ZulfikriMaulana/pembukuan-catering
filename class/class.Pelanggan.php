f788a68364be8a2ca9ff339d3d290ec0ec8fd303
Vebe <fikri1838@gmail.com>
2023-02-20 12:40:33 +0700

    Update Class Pelanggan Select

--- class/class.Pelanggan.php
+++ class/class.Pelanggan.php
@@ -74,7 +74,7 @@ class Pelanggan extends Connection
 	public function LihatSatuPelanggan() //belum
 	{
 		$this->connect();
-		$sql = "SELECT u.*, FROM pelanggan order by id_pelanggan = $this->id_pelanggan";
+		$sql = "SELECT * FROM pelanggan order by id_pelanggan = $this->id_pelanggan";
 
 		$resultOne = mysqli_query($this->connection, $sql) or die(mysqli_error($this->connection));
 
@@ -92,7 +92,7 @@ class Pelanggan extends Connection
 	public function LihatSemuaPelanggan() //Belum
 	{
 		$this->connect();
-		$sql = "SELECT u.*, FROM pelanggan order by id_pelanggan";
+		$sql = "SELECT * FROM pelanggan order by id_pelanggan";
 		$result = mysqli_query($this->connection, $sql) or die(mysqli_error($this->connection));
 
 		$arrResult = array();
