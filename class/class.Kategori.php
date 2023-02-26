29335597598d82a485ed223028ea47a9f755d1fb
Vebe <fikri1838@gmail.com>
2023-02-27 00:01:34 +0700

    Update class KAtegori

--- class/class.Kategori.php
+++ class/class.Kategori.php
@@ -43,4 +43,32 @@ class Kategori extends Connection
 		}
 		return $arrResult;
 	}
+
+	public function LihatSatuKategori()
+	{
+		$this->connect();
+		$sql = "SELECT * FROM kategori
+				WHERE id_kategori = $this->id_kategori";
+
+		$resultOne = mysqli_query($this->connection, $sql) or die(mysqli_error($this->connection));
+
+		if (mysqli_num_rows($resultOne) == 1) {
+			$this->hasil = true;
+			$data = mysqli_fetch_assoc($resultOne);
+			$this->id_kategori = $data['id_kategori'];
+			$this->nama_kategori = $data['nama_kategori'];
+			$this->jenis = $data['jenis'];
+		}
+	}
+
+	public function HapusKategori()
+	{
+		$this->connect();
+		$sql = "DELETE FROM kategori WHERE id_kategori=$this->id_kategori";
+		$this->hasil = mysqli_query($this->connection, $sql);
+		if ($this->hasil)
+			$this->message = 'Kategori berhasil dihapus!';
+		else
+			$this->message = 'Kategori gagal dihapus!';
+	}
 }
