35b417058ff99e56749a6201902713176b67481a
Vebe <fikri1838@gmail.com>
2023-02-26 13:24:29 +0700

    Update Tambah Transaksi

--- pages/tambahtransaksi.php
+++ pages/tambahtransaksi.php
@@ -1,3 +1,29 @@
+<?php
+require_once('./class/class.Transaksi.php');
+require_once('./class/class.Pelanggan.php');
+require_once('./class/class.ItemPesanan.php');
+
+$objPesanan = new Pesanan();
+
+if (isset($_POST['btnSubmit'])) {
+  $objTransaksi->id_transaksi = $_POST['id_transaksi'];
+  $objTransaksi->id_pesanan = $_POST['id_pesanan'];
+  $objTransaksi->id_kategori = $_POST['id_kategori'];
+  $objTransaksi->tanggal_transaksi = $_POST['tanggal_transaksi'];
+  $objTransaksi->jenis_transaksi = $_POST['jenis_transaksi'];
+  $objTransaksi->keterangan_transaksi = $_POST['keterangan_transaksi'];
+  $objTransaksi->foto_transaksi = $_POST['foto_transaksi'];
+  $objTransaksi->nominal_transaksi = $_POST['nominal_transaksi'];
+
+  $objTransaksi->TambahTransaksi();
+
+  echo "<script> alert('$objTransaksi->message'); </script>";
+  if ($objTransaksi->hasil) {
+    echo '<script> window.location="dashboard.php?p=lihattransaksi"; </script>';
+  }
+}
+?>
+
 <section class="content-header">
   <h1>
     Pesanan
@@ -21,11 +47,11 @@
 
           <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
 
-<!--------------------------------------------------------------------------------------------------->
+            <!--------------------------------------------------------------------------------------------------->
 
-          <div class="col-md-5"><!--COLUMN-->
-            
-            <div class="row"><!--ROW ID Pesanan-->
+            <div class="col-md-5"><!--COLUMN-->
+
+              <div class="row"><!--ROW ID Pesanan-->
                 <div class="form-group">
                   <label class="control-label col-sm-5" for="id_transaksi">ID Transaksi:</label>
                   <div class="col-sm-7">
@@ -48,7 +74,7 @@
                   <label class="control-label col-sm-5" for="jenis_transaksi">Jenis:</label>
                   <div class="col-sm-7">
                     <select class="form-control" id="jenis_transaksi" name="jenis_transaksi" required>
-                    <option value="1">Pemasukan</option>
+                      <option value="1">Pemasukan</option>
                     </select>
                   </div>
                 </div>
@@ -59,13 +85,13 @@
                   <label class="control-label col-sm-5" for="id_kategori">Kategori:</label>
                   <div class="col-sm-7">
                     <select class="form-control" id="id_kategori" name="id_kategori" required>
-                    <!--<option value="1">Pelunasan</option>-->
-                    <?php
+                      <!--<option value="1">Pelunasan</option>-->
+                      <?php
                       $objKategori = new Kategori();
                       $KategoriList = $objKategori->LihatSemuaKategori();
-                      foreach ($KategoriList as $Kategori){ 										
-                          echo '<option value='.$Kategori->id_kategori.'>'.$Kategori->nama_kategori.'</option>';
-                       }
+                      foreach ($KategoriList as $Kategori) {
+                        echo '<option value=' . $Kategori->id_kategori . '>' . $Kategori->nama_kategori . '</option>';
+                      }
                       ?>
                     </select>
                   </div>
@@ -85,7 +111,7 @@
                 <div class="form-group">
                   <label class="control-label col-sm-5" for="keterangan_transaksi">Keterangan:</label>
                   <div class="col-sm-7">
-                  <textarea class="form-control" id="keterangan_transaksi" name="keterangan_transaksi" rows="2" cols="20"></textarea>
+                    <textarea class="form-control" id="keterangan_transaksi" name="keterangan_transaksi" rows="2" cols="20"></textarea>
                   </div>
                 </div>
               </div>
@@ -100,19 +126,19 @@
               </div>
 
               <div class="row"><!--ROW Button-->
-            <div class="form-group">
-              <div class="col-sm-offset-5 col-sm-7">
-              <input type="submit" class="btn btn-success" value="Simpan" name="btnSubmit">
-              <a href="dashboard.php?p=lihatpesanan" class="btn btn-danger">Batal</a></td>
+                <div class="form-group">
+                  <div class="col-sm-offset-5 col-sm-7">
+                    <input type="submit" class="btn btn-success" value="Simpan" name="btnSubmit">
+                    <a href="dashboard.php?p=lihatpesanan" class="btn btn-danger">Batal</a></td>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
 
               <!----------------------------------------------------------------------------------------------->
 
-             
-        </div>
-        </form>
-</section>
-</div>
+
+            </div>
+          </form>
+    </section>
+  </div>
 </section>
