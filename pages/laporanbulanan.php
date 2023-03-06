a1c39af47ad8d57e66d4e03d3d545b8ab74d495a
TB Nawaf Musyaffa <iatmanegara.22@gmail.com>
2023-03-06 19:25:31 +0700

    Update laporanbulanan.php
    - Memperbaiki format bulan pada tabel tampilkan laporan bulanan

--- pages/laporanbulanan.php
+++ pages/laporanbulanan.php
@@ -49,20 +49,6 @@
                 <div class="form-group">
                   <label>Bulan :</label>
                   <input type="month" id="bulan" name="bulan" class="form-control">
-                  <!-- <select id="bulan" name="bulan" class="form-control">
-                      <option value="01">Januari</option>
-                      <option value="02">Februari</option>
-                      <option value="03">Maret</option>
-                      <option value="04">April</option>
-                      <option value="05">Mei</option>
-                      <option value="06">Juni</option>
-                      <option value="07">Juli</option>
-                      <option value="08">Agustus</option>
-                      <option value="09">September</option>
-                      <option value="10">Oktober</option>
-                      <option value="11">November</option>
-                      <option value="12">Desember</option>
-                    </select> -->
                 </div>
 
               </div>
@@ -108,7 +94,11 @@
           if (isset($_POST['btnSubmit'])) {
 
             $bulan = $_POST['bulan'];
+            $pecahanbulan = explode('-',$bulan );
+            echo $pecahanbulan[0]; // tahun
+            echo $pecahanbulan[1]; // bulan
             $id_kategori = $_POST['kategori'];
+            
           ?>
 
             <div class="row">
@@ -132,7 +122,7 @@
                       '11' => 'NOVEMBER',
                       '12' => 'DESEMBER',
                     );
-                    echo $namabulan[date('m')]; 
+                    echo $namabulan[$pecahanbulan[1]] .' '.$pecahanbulan[0]; 
                     ?>
                     </td>
                   </tr>
