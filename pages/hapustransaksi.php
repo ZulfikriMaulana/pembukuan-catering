4c1b7321c6e64c577f3c5f0ffef3d5075bf50f01
ahlijatinuraminah <ahlijati.nuraminah@gmail.com>
2023-03-29 14:28:49 +0700

    update hapus transaksi file juga dihapus

--- pages/hapustransaksi.php
+++ pages/hapustransaksi.php
@@ -3,10 +3,18 @@ if (isset($_GET['id_transaksi'])) {
 	require_once('./class/class.Transaksi.php');
 	$objTransaksi = new Transaksi();
 	$objTransaksi->id_transaksi = $_GET['id_transaksi'];
+	$objTransaksi->LihatSatuTransaksi();
+	$folder = './bukti/';
 
-	$objTransaksi->HapusTransaksi();
-	echo "<script> alert('$objTransaksi->message'); </script>";
-	echo "<script>window.location = 'dashboard.php?p=lihattransaksi'</script>";
+	if(file_exists($folder .'/'.$objTransaksi->foto_transaksi)){
+		unlink($folder .'/'.$objTransaksi->foto_transaksi);
+		$objTransaksi->HapusTransaksi();
+
+		if($objTransaksi->hasil){
+			echo "<script> alert('$objTransaksi->message'); </script>";
+			echo "<script>window.location = 'dashboard.php?p=lihattransaksi'</script>";
+		}
+	}
 } else {
 	echo '<script>window.history.back()</script>';
 }
